bb8b47a20b4a4fb3456f9cf263ebfdf3ea40ae7c
adamage <csernus.adam@promabit.com>
2023-10-03 11:38:55 +0200

    python solution 5

--- EchoCharacteristic.py
+++ EchoCharacteristic.py
@@ -17,13 +17,14 @@ class EchoCharacteristic(Characteristic):
         self._updateValueCallback = None
 
     def onReadRequest(self, offset, callback):
-        value = [hex(c) for c in self._value]
-        received_text = value.decode('utf-8')
-        print('EchoCharacteristic - %s - onReadRequest: value = %s' % (self['uuid'], received_text))
+        print('EchoCharacteristic - %s - onReadRequest: value = %s' % (self['uuid'], [hex(c) for c in self._value]))
         callback(Characteristic.RESULT_SUCCESS, self._value[offset:])
 
     def onWriteRequest(self, data, offset, withoutResponse, callback):
         self._value = data
+
+        hexValue = [hex(c) for c in self._value]
+        received_text = hexValue.decode('utf-8')
 
         print('EchoCharacteristic - %s - onWriteRequest: value = %s' % (self['uuid'], [hex(c) for c in self._value]))
 

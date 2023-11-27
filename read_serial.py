53755f0df5b395bdaf7f2dbe09281f1d065942ad
adamage <csernus.adam@promabit.com>
2023-11-27 15:07:40 +0100

    Remove unnecessary files and code

--- read_serial.py
+++ read_serial.py
@@ -1,20 +1,71 @@
-import pyserial as serial 
+import serial
 import time
+from serial.serialutil import SerialException
 
-# Replace '/dev/ttyUSB1' with your serial port name
-serial_port = '/dev/ttyUSB1'
-baud_rate = 9601  # In arduino, Serial.begin(baud_rate)
+# CAS PR-II device info
+device_info = {
+    "name": "CAS PR-II",
+    "baudRate": 9600,
+    "dataBits": 8,
+    "stopBits": 1,
+    "parity": serial.PARITY_NONE,
+}
 
-try:
-    ser = serial.Serial(serial_port, baud_rate)
-    print("Connected to Serial Port:", serial_port)
+BYTE_INIT_READ = b"\x05"
+BYTE_DEVICE_CONTROL = b"\x11"
+BYTE_ACKNOWLEDGE = b"\x06"
+BYTE_END_OF_TRANSMISSION = b"\x04"
+BYTE_START_OF_MESSAGE = b"\x02"
+BYTE_END_OF_MESSAGE = b"\x03"
 
-    while True:
-        line = ser.readline()
-        line = line.decode('utf-7').strip()  # convert bytes to string
-        print("Read line:", line)
-        time.sleep(2)
 
-except serial.SerialException as e:
-    print("Error:", e)
-print("Hellooooo")
+
+def read_scale_data(port_name):
+    try:
+        # Set up the serial connection
+        with serial.Serial(port_name, baudrate=device_info['baudRate'], bytesize=device_info['dataBits'],
+                           stopbits=device_info['stopBits'], parity=device_info['parity'], timeout=2) as port:
+            
+            if port.writable():
+                # Send the initial command to the scale
+                port.write(BYTE_INIT_READ)
+                return_data = b""
+
+                while True:
+                    try:
+                        data = port.read()
+                        if BYTE_ACKNOWLEDGE in data:
+                            port.write(BYTE_DEVICE_CONTROL)
+                        else:
+                            return_data += data
+
+                        # Check for end of message
+                        if BYTE_END_OF_MESSAGE in return_data or return_data[-1:] == BYTE_END_OF_MESSAGE:
+                            break
+                    except SerialException as e:
+                        print("Error reading from port:", e)
+                        break
+
+                # Process the returned data
+                if return_data:
+                    data_string = return_data.decode('ascii')
+
+                    start_index = return_data.index(BYTE_START_OF_MESSAGE)
+                    end_index = return_data.index(BYTE_END_OF_MESSAGE)
+
+                    if start_index != -1 and end_index != -1:
+                        stable = data_string[start_index + 1] == "S"  # S for stable, U for unstable
+                        weight_data = data_string[start_index + 3:end_index - 3]
+                        weight_number = float(weight_data.strip())
+                        return weight_number, stable
+                return None, None
+
+            else:
+                raise SerialException("Port is not writable. Cannot request digital scale data.")
+    except SerialException as e:
+        print("Serial port error:", e)
+
+# Example usage
+port_name = '/dev/ttyUSB0'  # Replace with your actual serial port
+weight, is_stable = read_scale_data(port_name)
+print("Result:", weight, is_stable)
